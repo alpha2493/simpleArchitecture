5e7dd2de0ac1c3a7d0d3fb1ac2aee7d046a1cb65
asi1024 <akifumiimanishi@yahoo.co.jp>
2014-04-02 18:54:21 +0900

    Change tab to space

--- src/ProgramCounter.v
+++ src/ProgramCounter.v
@@ -1,18 +1,18 @@
 module ProgramCounter (
-   input 	 load, reset, clk,
+   input         load, reset, clk,
    input [15:0]  address,
    output [15:0] counter);
-
-   reg [15:0] 	 count = 16'0;
-
+   
+   reg [15:0]    count = 16'0;
+   
    always @ (posedge clk) begin
       if (reset)
-	count = 16'0;
+	      count = 16'0;
       else if (load)
-	count = address;
+	      count = address;
       else
-	count = count + 1;
+	      count = count + 1;
    end
-
+   
    assign counter = count;
 endmodule
