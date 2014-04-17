27b140fec330f18f7898985f5a2c4c942e902925
kazakami <kazakami@kmc.gr.jp>
2014-04-17 15:30:12 +0900

    modify ControlUnit and ControlUnitTest

--- test/ControlUnitTest.v
+++ test/ControlUnitTest.v
@@ -1,19 +1,19 @@
 module CONTROLUNITTEST;
    reg [15:0] command;
    reg 	      clock;
-   wire       s_alu;
+   wire [3:0] s_alu;
 
    ControlUnit cu (clock, 0, command, s_alu);
 
    initial begin
       $dumpfile("cuTest.vcd");
-      $dumpvars(0, ControlUnitTest);
+      $dumpvars(0, CONTROLUNITTEST);
       $monitor("%t", command, s_alu);
 
       clock = 0;
-      command = 2'b1111110100001111;
+      command = 16'b1111110100001111;
       #10 clock = 1;
-      #10 clock = 0; command = 2'b0011110100000000;
+      #10 clock = 0; command = 16'b0011110100000000;
       #10 clock = 1;
       #10 $finish;
 
