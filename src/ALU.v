539663c08f861b4f23ab12a41661260f158db8b3
asi1024 <akifumiimanishi@yahoo.co.jp>
2014-04-18 12:22:40 +0900

    Little changes

--- src/ALU.v
+++ src/ALU.v
@@ -32,28 +32,20 @@ module ALU (
          INON : result = 16'b0;
          default : result = 16'b0;
       endcase // case S_ALU
-      if (S_ALU != INON)
-         begin
-            Z = result[15:0] == 1'b0;
-            C = result[16];
-            S = result[15] == 1'b1;
-            if (((S_ALU == IADD) 
-                 && (DATA_A[15] == DATA_B[15])
-                 && (DATA_A[15] != result[15]))
-                || ((S_ALU == ISUB)
-                    && (DATA_A[15] != DATA_B[15])
-                    && (DATA_A[15] != result[15])))
-              V = 1'b1;
-            else
-              V = 1'b0;
-         end
+
+      Z = result[15:0] == 1'b0;
+      C = result[16];
+      S = result[15] == 1'b1;
+      if (((S_ALU == IADD) 
+           && (DATA_A[15] == DATA_B[15])
+           && (DATA_A[15] != result[15]))
+          || ((S_ALU == ISUB)
+              && (DATA_A[15] != DATA_B[15])
+              && (DATA_A[15] != result[15])))
+        V = 1'b1;
       else
-        begin
-           Z = 0;
-           C = 0;
-           S = 0;
-           V = 0;
-        end
+        V = 1'b0;
+
    end // initial begin
 
    assign ALU_OUT = result[15:0];
