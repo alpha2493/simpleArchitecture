a181d7adf3d21f467203b9c75329bf3fe1ddb6e2
toyukan6 <toyukan6@gmail.com>
2014-04-18 13:11:11 +0900

    simplify alu

--- src/ALU.v
+++ src/ALU.v
@@ -13,49 +13,62 @@ module ALU (
    integer ISLR = 4'b1001;
    integer ISRL = 4'b1010;
    integer ISRA = 4'b1011;
+   integer IIDT = 4'b1100;
    integer INON = 4'b1111;
 
-   reg [16:0]    result;
-   reg           S,Z,C,V;
+   function [16:0] amux(
+      input signed [15:0] A, B,
+      input [3:0] Selector);
 
-   always @ (DATA_A or DATA_B or S_ALU) begin
-      case (S_ALU)
-         IADD : result = {1'b0, DATA_A} + {1'b0, DATA_B};
-         ISUB : result = {1'b0, DATA_A} - {1'b0, DATA_B};
-         IAND : result = {1'b0, DATA_A & DATA_B};
-         IOR  : result = {1'b0, DATA_A | DATA_B};
-         IXOR : result = {1'b0, DATA_A ^ DATA_B};
-         ISLL : result = {1'b0, DATA_A} << DATA_B[3:0];
-         ISLR : result = ({1'b0, DATA_A} << DATA_B[3:0]) | (DATA_A >> 16 - DATA_B[3:0]);
-         ISRL : result = {DATA_B[3:0] > 0 ? DATA_A[DATA_B[3:0] - 1] : 1'b0, DATA_A >> DATA_B[3:0]};
-         ISRA : result = {DATA_B[3:0] > 0 ? DATA_A[DATA_B[3:0] - 1] : 1'b0, DATA_A >>> DATA_B[3:0]};
-         INON : result = 16'b0;
-         default : result = 16'b0;
-      endcase // case S_ALU
-      if (S_ALU != INON)
-         begin
-            Z = result[15:0] == 1'b0;
-            C = result[16];
-            S = result[15] == 1'b1;
-            if (((S_ALU == IADD) 
-                 && (DATA_A[15] == DATA_B[15])
-                 && (DATA_A[15] != result[15]))
-                || ((S_ALU == ISUB)
-                    && (DATA_A[15] != DATA_B[15])
-                    && (DATA_A[15] != result[15])))
-              V = 1'b1;
-            else
-              V = 1'b0;
-         end
-      else
-        begin
-           Z = 0;
-           C = 0;
-           S = 0;
-           V = 0;
-        end
-   end // initial begin
+      case (Selector)
+         IADD : amux = {1'b0, A} + {1'b0, B};
+         ISUB : amux = {1'b0, A} - {1'b0, B};
+         IAND : amux = {1'b0, A & B};
+         IOR  : amux = {1'b0, A | B};
+         IXOR : amux = {1'b0, A ^ B};
+         ISLL : amux = {1'b0, A} << B[3:0];
+         ISLR : amux = ({1'b0, A} << B[3:0]) | (A >> 16 - B[3:0]);
+         ISRL : amux = {B[3:0] > 0 ? A[B[3:0] - 1] : 1'b0, A >> B[3:0]};
+         ISRA : amux = {B[3:0] > 0 ? A[B[3:0] - 1] : 1'b0, A >>> B[3:0]};
+         IIDT : amux = {1'b0, A};
+         INON : amux = 17'b0;
+      endcase 
+   endfunction
+  
+   wire [16:0]    result;
+   wire [19:0]    flags;
+   wire [16:0]    izero = 17'b0;
 
-   assign ALU_OUT = result[15:0];
-   assign FLAG_OUT = {S, Z, C, V};
+   MakeFlag mkf (DATA_A, DATA_B, result, S_ALU, flags);
+
+   assign result = amux(DATA_A, DATA_B, S_ALU);
+   assign ALU_OUT = flags[15:0];
+   assign FLAG_OUT = flags[19:16];
+endmodule
+
+module MakeFlag(
+       input [15:0] DATA_A, DATA_B,
+       input [16:0] result,
+       input [3:0] S_ALU, 
+       output [19:0] out);
+
+   integer IADD = 4'b0000;
+   integer ISUB = 4'b0001;
+   integer IAND = 4'b0010;
+   integer IOR  = 4'b0011;
+   integer IXOR = 4'b0100;
+   integer ISLL = 4'b1000;
+   integer ISLR = 4'b1001;
+   integer ISRL = 4'b1010;
+   integer ISRA = 4'b1011;
+   integer IIDT = 4'b1100;
+   integer INON = 4'b1111;
+
+   wire Z, S, V;
+   
+   assign S = result[15] == 1'b1 ? 1 : 0;
+   assign Z = result[15:0] == 16'b0 ? 1 : 0;
+   assign V = (((S_ALU == IADD) && (DATA_A[15] == DATA_B[15]) && (DATA_A[15] != result[15]))
+               || ((S_ALU == ISUB) && (DATA_A[15] != DATA_B[15]) && (DATA_A[15] != result[15]))) ? 1 : 0;
+   assign out = {S, Z, V, result};
 endmodule
