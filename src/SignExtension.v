a30ce29ca8052daf16519c8dad6ea723aaffda6c
toyukan6 <toyukan6@gmail.com>
2014-04-22 13:03:17 +0900

    hoge

--- src/SignExtension.v
+++ src/SignExtension.v
@@ -2,17 +2,20 @@ module SignExtension(I, O);
    parameter m = 4;
    parameter n = 16;
 
-   input [m-1:0] I;
-   output [n-1:0] O;
+   input [m - 1:0] I;
+   output [n - 1:0] O;
 
-   reg[n-1:0] d;
-   integer size = n - m;
+   reg[n - 1:0] d;
+   integer size;
    integer i;
+	
    always @ (I) begin
       d = I;
+		size = n - m;
       for (i = 0; i < size; i = i + 1)
-         d[n - 1 - i] <= I[m - 1];
-   end
+         d[n - 1 - i] = I[m - 1];
+   end 
+
    assign O = d;
 endmodule
 
@@ -20,17 +23,20 @@ module NotSignExtension(I, O);
    parameter m = 4;
    parameter n = 16;
 
-   input [m-1:0] I;
-   output [n-1:0] O;
+   input [m - 1:0] I;
+   output [n - 1:0] O;
 
-   reg[n-1:0] d;
-   integer size = n - m;
+   reg[n - 1:0] d;
+   integer size;
    integer i;
+	
    always @ (I) begin
       d = I;
+		size = n - m;
       for (i = 0; i < size; i = i + 1) 
-         d[n - 1 - i] <= 1'b0;
+         d[n - 1 - i] = 1'b0;
    end
+	
    assign O = d;
 endmodule
 
