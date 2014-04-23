14dea7cf0d32b790f2d060f6b6d75659a1a2f60e
asi1024 <akifumiimanishi@yahoo.co.jp>
2014-04-23 15:16:03 +0900

    Add Decode Unit Register

---
+++ src/CR2.v
@@ -0,0 +1,15 @@
+module OnewordRegister(
+   input  input_IN, wren_IN, CLK,
+   output input_OUT, wren_OUT);
+
+   reg    in, wren;
+
+   always @ (posedge CLK) begin
+      in <= input_IN;
+      wren <= wren_IN;
+   end
+   
+   assign input_OUT = in;
+   assign wren_OUT = wren;
+   
+endmodule // OnewordRegister
