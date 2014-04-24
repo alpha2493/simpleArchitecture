d5ea6d76e9a1842419783af42515909cf303a35b
MATSUOKA HIROSHI <a0126218@ws25.(none)>
2014-04-24 17:54:06 +0900

    debug JumJudgement.v

--- src/JumpJudgement.v
+++ src/JumpJudgement.v
@@ -11,12 +11,14 @@ module JumpJudgement(
       
    always @ (PC_load_in or cond) begin
       case (cond)
-	4'b000 : assign pcl = (Z && PC_load_in) ? 1'b1 : 1'b0;
-	4'b001 : assign pcl = ((S ^ V) && PC_load_in) ? 1'b1 : 1'b0;
-	4'b010 : assign pcl = ((Z || (S ^ V)) && PC_load_in) ? 1'b1 : 1'b0;
-	4'b011 : assign pcl = (!Z && PC_load_in) ? 1'b1 : 1'b0;
-	default : assign pcl = 1'b0;
+	4'b000 : pcl = (Z && PC_load_in) ? 1'b1 : 1'b0;
+	4'b001 : pcl = ((S ^ V) && PC_load_in) ? 1'b1 : 1'b0;
+	4'b010 : pcl = ((Z || (S ^ V)) && PC_load_in) ? 1'b1 : 1'b0;
+	4'b011 : pcl = (!Z && PC_load_in) ? 1'b1 : 1'b0;
+	default : pcl = 1'b0;
       endcase // case (cond)
    end
 
+	assign PC_load_out = pcl;
+	
 endmodule // JumpJudgement
