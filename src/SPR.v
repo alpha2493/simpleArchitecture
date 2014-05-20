16218251707d5c39d9242c79a2dcb874815772ce
MATSUOKA HIROSHI <a0126218@ws32.(none)>
2014-05-20 13:31:31 +0900

    modify SPR.v removing writeAdr and write.

--- src/SPR.v
+++ src/SPR.v
@@ -1,28 +1,22 @@
 module SPR(
-   input [15:0]  writeData,
-   input 	 inc, dec, write, clock, reset,
+   input 	 inc, dec,  clock, reset,
    output [15:0] Data);
 
    reg [15:0] 	 register;
 
-
-	
 	
    always @ (posedge clock or posedge reset) begin
-		if (reset)
-			register = 16'd0;
-		else
-		begin
-			if (inc)
-		register = register + 16'b1;
-	
-			if (dec)
-		register = register - 16'b1;
-	
-			if (write)
-		register = writeData;
-		end
+      if (reset)
+	register = 16'd0;
+      else
+	begin
+	   if (inc)
+	     register = register + 16'b1;
+	   
+	   if (dec)
+	     register = register - 16'b1;
+	end
    end
-
+   
    assign Data = register;
 endmodule // SPR
